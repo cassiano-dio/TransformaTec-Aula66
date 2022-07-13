f221f2f3b7f1f2047a4d35b2cafca0d3fd050b10
joaovictorprocopio <victor.345@hotmail.com>
2022-07-13 12:36:51 -0300

    add mais um Todos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Todos from './TodoList/Todos'
-import TrafficLight from './TrafficLights/TrafficLight';
-import './TrafficLights/styles.css'
-import FetchGithubUsers from './API/FetchGithubUsers';
+import "./App.css";
+import Todos from "./TodoList/Todos";
+import TrafficLight from "./TrafficLights/TrafficLight";
+import "./TrafficLights/styles.css";
+import FetchGithubUsers from "./API/FetchGithubUsers";
 
 function App() {
   return (
     <div className="App">
-      {/* <Todos /> */}
-      {/* <TrafficLight initialValue={0}/>
-      <TrafficLight initialValue={1}/> */}
+      <Todos />
+      <Todos />
+      <TrafficLight initialValue={0} />
+      <TrafficLight initialValue={1} />
       <FetchGithubUsers />
-      
     </div>
   );
 }
